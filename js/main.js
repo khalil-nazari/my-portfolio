c2740f6f6178027d136976ca5123ff2460265b7e
khalil Dev <khalil.deve@gmail.com>
2021-01-01 13:24:59 +0800

    New portfolio update

--- js/main.js
+++ js/main.js
@@ -1,64 +1,31 @@
+var swiper = new Swiper('#reactjs_projects', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
 
+   
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
 
-// portfolio links
-const pLinks = document.querySelectorAll('.p-links');
-const contentBody = document.querySelectorAll('.p-body');
-contentBody.forEach(el => {
-    if(el.attributes.id.value != 'home'){
-        el.style.display="none"
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 10,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 10,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 10,
+      },
     }
-});
-
-
-let pLinkId=[]; 
-pLinks.forEach((pLink) => {
-    btnToggle(pLinks, pLink);
-    pLinkId.push(pLink.attributes.id.value);     
-})
-
-
-
-
-
-// Toggle the buttons
-function btnToggle (pLinks, pLink) {
-    pLink.addEventListener('click', () => {
-        pLinks.forEach( (a) => {
-            a.classList.remove('link-btns-active')
-        })
-        pLink.classList.add('link-btns-active')
-        let id = pLink.attributes.id.value; 
-        contentToggle(contentBody, id)
-    })
-}
-
-function contentToggle (contentBody, id) {
-    contentBody.forEach(el => {
-        el.style.display="none"
-        if(el.attributes.id.value == id){
-            el.style.display="block"
-        }
-    });
-}
-
-
-// current year
-const newDate = new Date(); 
-const thisYear = newDate.getFullYear();
-document.getElementById("thisYear").innerHTML = thisYear
-
-
-// Scroll effect 
-let prevScrollpos = window.pageYOffset
-
-window.onscroll = () => {
-    let currentScrollPos = window.pageYOffset; 
-    if (prevScrollpos > currentScrollPos) {
-        document.querySelector('header').style.top = "0";
-      } else {
-        document.querySelector('header').style.top = "-120px";
-      }
-      prevScrollpos = currentScrollPos;
-
-
-}
+  });
