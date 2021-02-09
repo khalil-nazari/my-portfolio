fc893f9a4612cb43f28c52d698dbbeba385f69a2
khalil Dev <khalil.deve@gmail.com>
2021-02-10 03:22:08 +0800

    header and navbar is updated

--- js/main.js
+++ js/main.js
@@ -32,3 +32,28 @@ var swiper = new Swiper('#reactjs_projects', {
   });
 
 
+const navbar = document.getElementById("navbar")
+const headerarea = document.getElementById("header-area")
+window.onscroll = function () {
+  if(headerarea.offsetHeight - 5 < window.pageYOffset) {
+    navbar.style.backgroundColor="#013e5f"
+    navbar.style.color="#000"
+  } else {
+    navbar.style.backgroundColor="transparent"
+    navbar.style.color="#fff"
+  }
+}
+
+
+
+// Navbar Toggling
+document.getElementById("nav-btn")
+.addEventListener('click', (e)=>{
+  document.getElementById("nav").classList.toggle('show-nav') 
+})
+
+document.querySelectorAll("a").forEach((a)=>{
+  a.addEventListener('click', (e)=>{
+    document.getElementById("nav").classList.remove('show-nav') 
+  })
+})
